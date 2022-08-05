217102d5e094268df19f48efc7fdd762028ef6f0
Dasith U Edirisinghe <udasith@gmail.com>
2022-08-05 11:45:48 +0530

    Unwanted suffix removed

--- test/smoke_test_google-pegasus-xsum.py
+++ test/smoke_test_google-pegasus-xsum.py
@@ -22,7 +22,7 @@ class SmokeTest(unittest.TestCase):
 
     def testSum(self):
         self._waitForStartup()
-        url = 'http://localhost:8006/sum/'
+        url = 'http://localhost:8006/sum'
 
         req_body = {'text': 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.'}
         
@@ -62,7 +62,7 @@ class SmokeTest(unittest.TestCase):
 
     def testMeta(self):
         self._waitForStartup()
-        url = 'http://localhost:8006/meta/'
+        url = 'http://localhost:8006/meta'
         try:
             res = requests.get(url)
         except Exception as e:
